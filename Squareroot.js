a222f1f92ee08f4967b3b31012e8028316ceebf5
manan126 <manansharmaudz15@gmail.com>
2024-06-24 17:58:17 +0530

    q1 commit

---
+++ Squareroot.js
@@ -0,0 +1,7 @@
+const number = 16
+if (number < 0) {
+    console.log('Square root of a negative number is not real.');
+} else {
+    const squareRoot = Math.sqrt(number);
+    console.log(`The square root of ${number} is ${squareRoot}.`);
+}
