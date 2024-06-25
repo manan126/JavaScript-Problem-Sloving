8d2674192ac3cbe8c90f284827a8e4c2df133aae
manan126 <manansharmaudz15@gmail.com>
2024-06-25 19:26:47 +0530

    Question29

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+// Reverse a String
+
+
+let string = "Lelouch";
+
+
+// Split method convers array into array of sub string(NOTE : USE SINGLE INVERTED COMMAS FOR SPLITTING)
+let reverseString = string.split('').reverse()
+
+console.log(reverseString.toString())
